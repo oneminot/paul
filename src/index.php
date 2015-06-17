6925bb2611fcb4a6b2f7a7946e43f0355e4988a4
kushal <kushaldeveloper@gmail.com>
2015-06-17 16:29:53 -0500

    test database connection using code from http://php.net/manual/en/function.oci-connect.php
    success

--- src/index.php
+++ src/index.php
@@ -1,8 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: minot_000
- * Date: 6/17/2015
- * Time: 4:07 PM
- */
-echo 'hello';
+
+// Connects to the XE service (i.e. database) on the "localhost" machine
+$conn = oci_connect('', '', 'localhost/XE');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM USERS  ');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+?>
