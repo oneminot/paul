c41e1ce25077d6a2f84f9447b9731b1dfebc050a
kushal <kushaldeveloper@gmail.com>
2015-06-18 09:31:31 -0500

    check if changing publisher id after associating publisher with article works
    it works!!!1

--- src/index.php
+++ src/index.php
@@ -17,6 +17,8 @@ $my_article->relative_file_location = "p1.pdf";
 $my_article->page_reference = "p7-9";
 $my_article->publisher_id = $my_publisher;
 
+$my_publisher->id = 99;
+
 $my_author = new Author();
 $my_author->id = 1;
 $my_author->name = "Professor Plum";
@@ -38,40 +40,3 @@ $your_author_article->sequence_number = 2;
 var_dump($my_author_article);
 var_dump($your_author_article);
 
-// result of var dump
-//object(Author_Article)[5]
-//  public 'author_id' =>
-//    object(Author)[3]
-//      public 'id' => int 1
-//      public 'name' => string 'Professor Plum' (length=14)
-//  public 'article_id' =>
-//    object(Article)[2]
-//      public 'id' => int 1
-//      public 'title' => string 'Article title' (length=13)
-//      public 'relative_file_location' => string 'p1.pdf' (length=6)
-//      public 'publisher_id' =>
-//        object(Publisher)[1]
-//          public 'id' => int 1
-//          public 'title' => string 'Pearson' (length=7)
-//          public 'location' => string 'New York' (length=8)
-//          public 'year' => int 2004
-//      public 'page_reference' => string 'p7-9' (length=4)
-//  public 'sequence_number' => int 2
-//object(Author_Article)[6]
-//  public 'author_id' =>
-//    object(Author)[3]
-//      public 'id' => int 1
-//      public 'name' => string 'Professor Plum' (length=14)
-//  public 'article_id' =>
-//    object(Article)[2]
-//      public 'id' => int 1
-//      public 'title' => string 'Article title' (length=13)
-//      public 'relative_file_location' => string 'p1.pdf' (length=6)
-//      public 'publisher_id' =>
-//        object(Publisher)[1]
-//          public 'id' => int 1
-//          public 'title' => string 'Pearson' (length=7)
-//          public 'location' => string 'New York' (length=8)
-//          public 'year' => int 2004
-//      public 'page_reference' => string 'p7-9' (length=4)
-//  public 'sequence_number' => int 2
