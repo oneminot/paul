c2cc9234464036a1cd55b9d47de7f946806239a4
kushal <kushaldeveloper@gmail.com>
2015-07-04 22:04:23 -0500

    tweak example code

--- display_admin.php
+++ display_admin.php
@@ -32,13 +32,17 @@ include_once("display_navigation_bar.php");
     </form>
     <?php
     if (isset($_POST['k_username']) && isset($_POST['k_password'])) {
-        $user_input = htmlspecialchars($_POST['k_password']);
+        $user_input_password = htmlspecialchars($_POST['k_password']);
+        $user_input_username = htmlspecialchars(trim($_POST['k_username']));
+        $name_fields = '/[a-zA-Z\s]+/'; // alpha or spaces, at least 1 char
+        $string = "Match this string";
+        var_dump(filter_var($user_input_username, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/[a-zA-Z\s]+/'))));
         // let the salt be automatically generated
         $hashed_password = password_hash($_POST['k_password'], PASSWORD_BCRYPT);
         $admin_user = new User();
         $admin_user->password = $hashed_password;
         echo "<pre>" . var_export($admin_user, true) . "</pre>";
-        if (password_verify($user_input, $hashed_password)) {
+        if (password_verify(htmlspecialchars($user_input_password), $hashed_password)) {
             echo "password is valid";
         } else {
             echo "password is invalid";
