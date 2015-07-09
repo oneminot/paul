d9707c9f0bb84bbe298f6f438a5e47b1c3285926
kushal <kushaldeveloper@gmail.com>
2015-07-09 17:20:31 -0500

    create a test
    for some reason it won't work
    
    it says
    
    $ phpunit --bootstrap vendor/autoload.php tests/UserTest.php
    PHPUnit 4.7.6 by Sebastian Bergmann and contributors.
    
    F
    
    Time: 330 ms, Memory: 13.50Mb
    
    There was 1 failure:
    
    1) Warning
    No tests found in class "UserTest".
    
    FAILURES!
    Tests: 1, Assertions: 0, Failures: 1.

---
+++ tests/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kusha_000
+ * Date: 7/9/2015
+ * Time: 5:03 PM
+ */
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    function __autoload($class_name)
+    {
+        include_once 'class.' . $class_name . '.inc';
+        include_once 'vendor/autoload.php';
+    }
+
+    public function verify_password_works()
+    {
+        $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
+        $admin_user = new \User();
+        $admin_user->set_password($my_password);
+        $admin_user->attempt_count = 0;
+        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+        $this->assertTrue($admin_user->verify_password($my_password));
+    }
+}
