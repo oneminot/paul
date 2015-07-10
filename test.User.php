4190f6aa92873178643931dcd171fc3681d0522e
kushal <kushaldeveloper@gmail.com>
2015-07-10 12:03:18 -0500

    move test user
    [05:03 PM]-[vagrant@local]-[/var/www/awesome/paul]-[git master]
    $ phpunit --bootstrap vendor/autoload.php test.User.php
    PHPUnit 4.7.6 by Sebastian Bergmann and contributors.
    
    PHP Fatal error:  Class 'User' not found in /var/www/awesome/paul/test.User.php on line 20
    PHP Stack trace:
    PHP   1. {main}() /usr/local/bin/phpunit:0
    PHP   2. PHPUnit_TextUI_Command::main() /usr/local/bin/phpunit:537
    PHP   3. PHPUnit_TextUI_Command->run() phar:///usr/local/bin/phpunit/phpunit/TextUI/Command.php:99
    PHP   4. PHPUnit_TextUI_TestRunner->doRun() phar:///usr/local/bin/phpunit/phpunit/TextUI/Command.php:147
    PHP   5. PHPUnit_Framework_TestSuite->run() phar:///usr/local/bin/phpunit/phpunit/TextUI/TestRunner.php:428
    PHP   6. PHPUnit_Framework_TestCase->run() phar:///usr/local/bin/phpunit/phpunit/Framework/TestSuite.php:738
    PHP   7. PHPUnit_Framework_TestResult->run() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:702
    PHP   8. PHPUnit_Framework_TestCase->runBare() phar:///usr/local/bin/phpunit/phpunit/Framework/TestResult.php:601
    PHP   9. PHPUnit_Framework_TestCase->runTest() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:746
    PHP  10. ReflectionMethod->invokeArgs() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:881
    PHP  11. UserTest->test_verify_password_works() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:881
    
    Fatal error: Class 'User' not found in /var/www/awesome/paul/test.User.php on line 20
    
    Call Stack:
        0.0007     394328   1. {main}() /usr/local/bin/phpunit:0
        0.0672   13165952   2. PHPUnit_TextUI_Command::main() /usr/local/bin/phpunit:537
        0.0672   13166576   3. PHPUnit_TextUI_Command->run() phar:///usr/local/bin/phpunit/phpunit/TextUI/Command.php:99
        0.3166   13500288   4. PHPUnit_TextUI_TestRunner->doRun() phar:///usr/local/bin/phpunit/phpunit/TextUI/Command.php:147
        0.3192   13510920   5. PHPUnit_Framework_TestSuite->run() phar:///usr/local/bin/phpunit/phpunit/TextUI/TestRunner.php:428
        0.3217   13516296   6. PHPUnit_Framework_TestCase->run() phar:///usr/local/bin/phpunit/phpunit/Framework/TestSuite.php:738
        0.3217   13517944   7. PHPUnit_Framework_TestResult->run() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:702
        0.3218   13520664   8. PHPUnit_Framework_TestCase->runBare() phar:///usr/local/bin/phpunit/phpunit/Framework/TestResult.php:601
        0.3221   13558280   9. PHPUnit_Framework_TestCase->runTest() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:746
        0.3221   13559056  10. ReflectionMethod->invokeArgs() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:881
        0.3221   13559208  11. UserTest->test_verify_password_works() phar:///usr/local/bin/phpunit/phpunit/Framework/TestCase.php:881

--- test.User.php
+++ test.User.php
@@ -14,10 +14,10 @@ class UserTest extends \PHPUnit_Framework_TestCase
         include_once 'vendor/autoload.php';
     }
 
-    public function verify_password_works()
+    public function test_verify_password_works()
     {
         $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
-        $admin_user = new \User();
+        $admin_user = new User();
         $admin_user->set_password($my_password);
         $admin_user->attempt_count = 0;
         $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
