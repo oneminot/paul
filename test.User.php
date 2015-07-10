93710cb7d111c27d1f3ed34228ade6697f9ceaa4
kushal <kushaldeveloper@gmail.com>
2015-07-10 12:25:30 -0500

    fail test
    so I can change code to correct it
    
    thanks, Uncle Bob
    
    [05:21 PM]-[vagrant@local]-[/var/www/awesome/paul]-[git master]
    $ phpunit --bootstrap vendor/autoload.php test.User.php
    PHPUnit 4.7.6 by Sebastian Bergmann and contributors.
    
    .F
    
    Time: 611 ms, Memory: 13.50Mb
    
    There was 1 failure:
    
    1) UserTest::test_verify_password_fails_if_attempt_count_exceeds_maximum
    Failed asserting that true is false.
    
    /var/www/awesome/paul/test.User.php:32
    
    FAILURES!
    Tests: 2, Assertions: 2, Failures: 1.

--- test.User.php
+++ test.User.php
@@ -10,6 +10,8 @@
 require_once "class.User.inc";
 class UserTest extends \PHPUnit_Framework_TestCase
 {
+    const TOTAL_NUMBER_OF_ATTEMPTS_ALLOWED = 5;
+    const TIME_BEFORE_ANOTHER_ATTEMPT_ALLOWED_IN_MINUTES = 2;
     public function test_verify_password_works()
     {
         $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
@@ -18,5 +20,15 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $admin_user->attempt_count = 0;
         $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
         $this->assertTrue($admin_user->verify_password($my_password));
+    }
+
+    public function test_verify_password_fails_if_attempt_count_exceeds_maximum()
+    {
+        $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
+        $admin_user = new User();
+        $admin_user->set_password($my_password);
+        $admin_user->attempt_count = 6;
+        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+        $this->assertFalse($admin_user->verify_password($my_password));
     }
 }
