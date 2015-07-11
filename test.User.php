591b74cf14e55d365363a9d120d1b3174306a2b7
kushal <kushaldeveloper@gmail.com>
2015-07-11 13:20:07 -0500

    add another test to User
    to make sure password verification fails for two different strings

--- test.User.php
+++ test.User.php
@@ -19,4 +19,14 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
         $this->assertTrue($admin_user->verify_password($my_password));
     }
+
+    public function test_verify_password_does_not_work()
+    {
+        $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
+        $admin_user = new User();
+        $admin_user->set_password("apple");
+        $admin_user->attempt_count = 0;
+        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+        $this->assertFalse($admin_user->verify_password($my_password));
+    }
 }
