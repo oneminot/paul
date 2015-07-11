5b41cdda8d2d8aa93bf336d6c7efc767e1092685
kushal <kushaldeveloper@gmail.com>
2015-07-11 13:21:35 -0500

    add test for password with a backslash
    it currently fails
    
    [06:19 PM]-[vagrant@local]-[/var/www/awesome/paul]-[git master]
    $ phpunit --bootstrap vendor/autoload.php test.User.php
    PHPUnit 4.7.6 by Sebastian Bergmann and contributors.
    
    ..F
    
    Time: 686 ms, Memory: 13.50Mb
    
    There was 1 failure:
    
    1) UserTest::test_verify_password_works_for_backslashes
    Failed asserting that true is false.
    
    /var/www/awesome/paul/test.User.php:40
    
    FAILURES!
    Tests: 3, Assertions: 3, Failures: 1.

--- test.User.php
+++ test.User.php
@@ -29,4 +29,14 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
         $this->assertFalse($admin_user->verify_password($my_password));
     }
+
+    public function test_verify_password_works_for_backslashes()
+    {
+        $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgt\BJFySImIZHF0OIBjnv7Q4SnBZf96";
+        $admin_user = new User();
+        $admin_user->set_password($my_password);
+        $admin_user->attempt_count = 0;
+        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+        $this->assertFalse($admin_user->verify_password($my_password));
+    }
 }
