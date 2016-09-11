5faa4a5f3c0cf8a08fc11b0878cc3b704b2c3bc1
kushal <kushaldeveloper@gmail.com>
2016-09-11 00:26:32 -0400

    fix php unit test?
    see https://github.com/sebastianbergmann/phpunit/issues/1953

--- Tests/UserTest.php
+++ Tests/UserTest.php
@@ -12,30 +12,52 @@ class UserTest extends \PHPUnit_Framework_TestCase
     public function test_verify_password_works()
     {
         $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
-        $admin_user = new kus\auth\User();
-        $admin_user->set_password($my_password);
-        $admin_user->attempt_count = 0;
-        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
-        $this->assertTrue($admin_user->verify_password($my_password));
+        try 
+        {
+            $admin_user = new kus\auth\User();
+            $admin_user->set_password($my_password);
+            $admin_user->attempt_count = 0;
+            $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+            $this->assertTrue($admin_user->verify_password($my_password));
+        }
+        catch (Throwable $e) 
+        {
+            throw new Exception($e);
+        }
     }
 
     public function test_verify_password_does_not_work()
     {
-        $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
-        $admin_user = new kus\auth\User();
-        $admin_user->set_password("apple");
-        $admin_user->attempt_count = 0;
-        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
-        $this->assertFalse($admin_user->verify_password($my_password));
+        try 
+        {
+            $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgtBJFySImIZHF0OIBjnv7Q4SnBZf96";
+            $admin_user = new kus\auth\User();
+            $admin_user->set_password("apple");
+            $admin_user->attempt_count = 0;
+            $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+            $this->assertFalse($admin_user->verify_password($my_password));
+        }
+        catch (Throwable $e) 
+        {
+            throw new Exception($e);
+        }
+
     }
 
     public function test_verify_password_works_for_backslashes()
     {
-        $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgt\BJFySImIZHF0OIBjnv7Q4SnBZf96";
-        $admin_user = new kus\auth\User();
-        $admin_user->set_password($my_password);
-        $admin_user->attempt_count = 0;
-        $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
-        $this->assertTrue($admin_user->verify_password($my_password));
+        try 
+        {
+            $my_password = "fh7ZVHma7tVEGKafmd99gybmOHK39as7qgt\BJFySImIZHF0OIBjnv7Q4SnBZf96";
+            $admin_user = new kus\auth\User();
+            $admin_user->set_password($my_password);
+            $admin_user->attempt_count = 0;
+            $admin_user->last_attempt = new DateTime('yesterday', new DateTimeZone('UTC'));
+            $this->assertTrue($admin_user->verify_password($my_password));
+        }
+        catch (Throwable $e) 
+        {
+            throw new Exception($e);
+        }    
     }
 }
